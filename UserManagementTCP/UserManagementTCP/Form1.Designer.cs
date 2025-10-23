051f38b839f4c533c62766f839a072a630549cbd
npyvyy <24522060@gm.uit.edu.vn>
2025-10-24 01:21:43 +0700

    Add login and register UI

---
+++ UserManagementTCP/UserManagementTCP/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace UserManagementTCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            btndk = new Button();
+            txtht = new TextBox();
+            txtsdt = new TextBox();
+            txte = new TextBox();
+            txtns = new TextBox();
+            txtmk = new TextBox();
+            txtnlmk = new TextBox();
+            label8 = new Label();
+            btndn = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(255, 128, 128);
+            label1.Location = new Point(184, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(450, 45);
+            label1.TabIndex = 0;
+            label1.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Times New Roman", 16.2F);
+            label2.ForeColor = Color.FromArgb(255, 192, 192);
+            label2.Location = new Point(116, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 33);
+            label2.TabIndex = 1;
+            label2.Text = "Họ tên";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Times New Roman", 16.2F);
+            label3.ForeColor = Color.FromArgb(255, 192, 192);
+            label3.Location = new Point(116, 207);
+            label3.Name = "label3";
+            label3.Size = new Size(163, 33);
+            label3.TabIndex = 2;
+            label3.Text = "Số điện thoại";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 16.2F);
+            label4.ForeColor = Color.FromArgb(255, 192, 192);
+            label4.Location = new Point(116, 273);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 33);
+            label4.TabIndex = 3;
+            label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Times New Roman", 16.2F);
+            label5.ForeColor = Color.FromArgb(255, 192, 192);
+            label5.Location = new Point(116, 341);
+            label5.Name = "label5";
+            label5.Size = new Size(127, 33);
+            label5.TabIndex = 4;
+            label5.Text = "Ngày sinh";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Times New Roman", 16.2F);
+            label6.ForeColor = Color.FromArgb(255, 192, 192);
+            label6.Location = new Point(116, 410);
+            label6.Name = "label6";
+            label6.Size = new Size(121, 33);
+            label6.TabIndex = 5;
+            label6.Text = "Mật khẩu";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Times New Roman", 16.2F);
+            label7.ForeColor = Color.FromArgb(255, 192, 192);
+            label7.Location = new Point(116, 478);
+            label7.Name = "label7";
+            label7.Size = new Size(218, 33);
+            label7.TabIndex = 6;
+            label7.Text = "Nhập lại mật khẩu";
+            // 
+            // btndk
+            // 
+            btndk.BackColor = Color.FromArgb(255, 128, 128);
+            btndk.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btndk.ForeColor = SystemColors.ControlText;
+            btndk.Location = new Point(447, 551);
+            btndk.Name = "btndk";
+            btndk.Size = new Size(225, 58);
+            btndk.TabIndex = 7;
+            btndk.Text = "ĐĂNG KÝ";
+            btndk.UseVisualStyleBackColor = false;
+            // 
+            // txtht
+            // 
+            txtht.Location = new Point(382, 124);
+            txtht.Multiline = true;
+            txtht.Name = "txtht";
+            txtht.Size = new Size(356, 49);
+            txtht.TabIndex = 8;
+            // 
+            // txtsdt
+            // 
+            txtsdt.Location = new Point(382, 191);
+            txtsdt.Multiline = true;
+            txtsdt.Name = "txtsdt";
+            txtsdt.Size = new Size(356, 49);
+            txtsdt.TabIndex = 9;
+            // 
+            // txte
+            // 
+            txte.Location = new Point(382, 257);
+            txte.Multiline = true;
+            txte.Name = "txte";
+            txte.Size = new Size(356, 49);
+            txte.TabIndex = 10;
+            // 
+            // txtns
+            // 
+            txtns.Location = new Point(382, 325);
+            txtns.Multiline = true;
+            txtns.Name = "txtns";
+            txtns.Size = new Size(356, 49);
+            txtns.TabIndex = 11;
+            // 
+            // txtmk
+            // 
+            txtmk.Location = new Point(382, 394);
+            txtmk.Multiline = true;
+            txtmk.Name = "txtmk";
+            txtmk.Size = new Size(356, 49);
+            txtmk.TabIndex = 12;
+            // 
+            // txtnlmk
+            // 
+            txtnlmk.Location = new Point(382, 462);
+            txtnlmk.Multiline = true;
+            txtnlmk.Name = "txtnlmk";
+            txtnlmk.Size = new Size(356, 49);
+            txtnlmk.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Times New Roman", 16.2F);
+            label8.ForeColor = Color.FromArgb(255, 192, 192);
+            label8.Location = new Point(12, 576);
+            label8.Name = "label8";
+            label8.Size = new Size(202, 33);
+            label8.TabIndex = 14;
+            label8.Text = "Đã có tài khoản?";
+            // 
+            // btndn
+            // 
+            btndn.ActiveLinkColor = Color.Red;
+            btndn.AutoSize = true;
+            btndn.BackColor = Color.Transparent;
+            btndn.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btndn.LinkBehavior = LinkBehavior.NeverUnderline;
+            btndn.LinkColor = Color.IndianRed;
+            btndn.Location = new Point(220, 580);
+            btndn.Name = "btndn";
+            btndn.Size = new Size(134, 26);
+            btndn.TabIndex = 15;
+            btndn.TabStop = true;
+            btndn.Text = "[Đăng Nhập]";
+            btndn.VisitedLinkColor = Color.Firebrick;
+            btndn.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Screenshot_2025_10_14_184430;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(830, 655);
+            Controls.Add(btndn);
+            Controls.Add(label8);
+            Controls.Add(txtnlmk);
+            Controls.Add(txtmk);
+            Controls.Add(txtns);
+            Controls.Add(txte);
+            Controls.Add(txtsdt);
+            Controls.Add(txtht);
+            Controls.Add(btndk);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button btndk;
+        private TextBox txtht;
+        private TextBox txtsdt;
+        private TextBox txte;
+        private TextBox txtns;
+        private TextBox txtmk;
+        private TextBox txtnlmk;
+        private Label label8;
+        private LinkLabel btndn;
+    }
+}
---
+++ UserManagementTCP/UserManagementTCP/Form2.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace UserManagementTCP
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            linkLabel1 = new LinkLabel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label4 = new Label();
+            linkLabel2 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(255, 128, 128);
+            label1.Location = new Point(184, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(479, 46);
+            label1.TabIndex = 0;
+            label1.Text = "ĐĂNG NHẬP TÀI KHOẢN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(255, 192, 192);
+            label2.Location = new Point(134, 154);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 33);
+            label2.TabIndex = 1;
+            label2.Text = "E-mail";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(255, 192, 192);
+            label3.Location = new Point(134, 240);
+            label3.Name = "label3";
+            label3.Size = new Size(121, 33);
+            label3.TabIndex = 2;
+            label3.Text = "Mật khẩu";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkBehavior = LinkBehavior.NeverUnderline;
+            linkLabel1.LinkColor = Color.IndianRed;
+            linkLabel1.Location = new Point(622, 331);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(165, 26);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Quên mật khẩu?";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(368, 141);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(346, 46);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(368, 227);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(346, 46);
+            textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(452, 410);
+            button1.Name = "button1";
+            button1.Size = new Size(227, 58);
+            button1.TabIndex = 6;
+            button1.Text = "ĐĂNG NHẬP";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(255, 192, 192);
+            label4.Location = new Point(134, 331);
+            label4.Name = "label4";
+            label4.Size = new Size(230, 33);
+            label4.TabIndex = 7;
+            label4.Text = "Chưa có tài khoản?";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = Color.Transparent;
+            linkLabel2.Font = new Font("Times New Roman", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel2.LinkBehavior = LinkBehavior.NeverUnderline;
+            linkLabel2.LinkColor = Color.IndianRed;
+            linkLabel2.Location = new Point(385, 331);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(112, 26);
+            linkLabel2.TabIndex = 8;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "[Đăng Ký]";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Screenshot_2025_10_14_184430;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(895, 552);
+            Controls.Add(linkLabel2);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private LinkLabel linkLabel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label4;
+        private LinkLabel linkLabel2;
+    }
+}
