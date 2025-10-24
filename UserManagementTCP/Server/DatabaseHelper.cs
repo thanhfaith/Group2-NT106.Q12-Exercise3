41ad36db3a16ebfbe30ad82b59c571050bb67ee2
Võ Diệp Thành <vodiepthanh06@gmail.com>
2025-10-24 17:22:04 +0700

    Update tcp socket and database

---
+++ UserManagementTCP/Server/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class DatabaseHelper
+    {
+        private static string connStr = "Data Source=DESKTOP-9Q6P0AS\\MSSQLSERVER01;Initial Catalog=UserDB;Integrated Security=True";
+
+        public static string GetAllUsers()
+        {
+            string result = "";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Username, Email FROM Users", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    result += $"{reader["Username"]} - {reader["Email"]}\n";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ UserManagementTCP/Server/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.lb_status = new System.Windows.Forms.Label();
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.tb_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(43, 40);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(174, 86);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // lb_status
+            // 
+            this.lb_status.AutoSize = true;
+            this.lb_status.Location = new System.Drawing.Point(340, 180);
+            this.lb_status.Name = "lb_status";
+            this.lb_status.Size = new System.Drawing.Size(74, 20);
+            this.lb_status.TabIndex = 1;
+            this.lb_status.Text = "lb_status";
+            // 
+            // lstClients
+            // 
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.ItemHeight = 20;
+            this.lstClients.Location = new System.Drawing.Point(500, 51);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.Size = new System.Drawing.Size(213, 104);
+            this.lstClients.TabIndex = 2;
+            // 
+            // tb_log
+            // 
+            this.tb_log.Location = new System.Drawing.Point(279, 265);
+            this.tb_log.Name = "tb_log";
+            this.tb_log.Size = new System.Drawing.Size(421, 26);
+            this.tb_log.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_log);
+            this.Controls.Add(this.lstClients);
+            this.Controls.Add(this.lb_status);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label lb_status;
+        private System.Windows.Forms.ListBox lstClients;
+        private System.Windows.Forms.TextBox tb_log;
+    }
+}
+
